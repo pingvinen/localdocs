96c191048b2181241259ae0366d36035a7834fcd
Patrick <pingvinen@isflagen.dk>
2013-01-19 01:55:57 -0500

    The sitemap now works for the default layout (it is not super pretty, but it works). fixes #5

--- src/web/Handlers/MarkdownSupport/Sitemap.cs
+++ src/web/Handlers/MarkdownSupport/Sitemap.cs
@@ -5,6 +5,11 @@ namespace LocalDocs.Web.Handlers.MarkdownSupport
 {
 	public class Sitemap
 	{
+		public Sitemap()
+		{
+			this.Entries = new List<SitemapEntry>();
+		}
+
 		public IList<SitemapEntry> Entries { get; set; }
 	}
 }
--- src/web/Handlers/SitemapPageHandler.cs
+++ src/web/Handlers/SitemapPageHandler.cs
@@ -1,11 +1,9 @@
 using System;
 using System.Web;
-using System.Text;
-using System.Collections.Generic;
-using System.IO;
-using MarkdownSharp;
-using LocalDocs.Web.SparkStuff;
 using LocalDocs.Web.Handlers.MarkdownSupport;
+using System.IO;
+using LocalDocs.Web.SparkStuff;
+using System.Collections.Generic;
 
 namespace LocalDocs.Web.Handlers
 {
@@ -36,7 +34,7 @@ namespace LocalDocs.Web.Handlers
 				throw new InvalidOperationException(String.Format("Sitemap template file for '{0}' is missing. It should be at '{1}'", target.Name, sitemapPath));
 			}
 
-			vm.Sitemap = this.GetSitemap(targetRootDir);
+			vm.Sitemap = this.BuildSitemap(targetRootDir);
 
 			SparkRenderer renderer = new SparkRenderer();
 			string sitemapOutput = renderer.Render(sitemapPath, vm);
@@ -48,9 +46,60 @@ namespace LocalDocs.Web.Handlers
 		}
 		#endregion
 
-		private Sitemap GetSitemap(string targetRootDir)
+		#region Build sitemap
+		private Sitemap BuildSitemap(string targetRootDir)
 		{
-			return new Sitemap();
+			Sitemap res = new Sitemap();
+
+			Queue<string> q = new Queue<string>();
+
+			q.Enqueue(targetRootDir);
+
+			string cur;
+			string webpath;
+			SitemapEntry entry;
+			while (q.Count > 0)
+			{
+				cur = q.Dequeue();
+
+				//
+				// get all the subdirectories
+				//
+				foreach (string s in Directory.GetDirectories(cur))
+				{
+					if (s.EndsWith(Constants.LayoutFolderName))
+					{
+						continue;
+					}
+
+					q.Enqueue(s);
+				}
+
+				//
+				// create the request path for the current dir
+				//
+				webpath = cur.Remove(0, targetRootDir.Length-1);
+				if (!webpath.EndsWith("/"))
+				{
+					webpath = String.Concat(webpath, "/");
+				}
+
+				//
+				// get all the markdown files
+				//
+				foreach (string s in Directory.GetFiles(cur, "*.md"))
+				{
+					entry = new SitemapEntry();
+					entry.Namespace = webpath;
+					entry.Name = Path.GetFileNameWithoutExtension(s);
+					entry.Url = String.Concat(webpath, entry.Name);
+
+					res.Entries.Add(entry);
+				}
+			}
+
+			return res;
 		}
+		#endregion Build sitemap
 	}
 }
